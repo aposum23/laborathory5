51b4e2b32d4ad97d676269d2fbc7f47829002917
aposum23 <81620056+aposum23@users.noreply.github.com>
2021-11-12 17:22:38 +0300

    Update hardLevel.py

--- hardLevel.py
+++ hardLevel.py
@@ -3,10 +3,11 @@ import math
 
 if __name__ == '__main__':
     x = float(input('Enter x: '))
-    a = math.pow(x * (-1), 3) / 3
+    a = x
     sum = a
     for n in range(1, 170):
-        a *= (math.pow(-1, n) * math.pow(x, 2 * n + 1) * 2 * math.pow(n + 1, 2)) / ((2 * n + 1) * math.pow(-1, n + 1) * math.pow(x, 2 * n + 2))
+        a *= (math.pow(-1, n) * math.pow(x, 2 * n + 1) * 2 * math.pow(n + 1, 2)) \
+                    / ((2 * n + 1) * math.pow(-1, n + 1) * math.pow(x, 2 * n + 2))
         sum += a
 
     res = 2 / math.sqrt(math.pi) * sum
