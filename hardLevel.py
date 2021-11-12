030e60a310fc33ec3dcaaeae5cfa67ca84e971fb
aposum23 <misha.duhno@mail.ru>
2021-11-12 16:53:51 +0300

    Refactoring

--- hardLevel.py
+++ hardLevel.py
@@ -3,12 +3,12 @@ import math
 
 if __name__ == '__main__':
     x = float(input('Enter x: '))
+    a = math.pow(x * (-1), 3) / 3
+    sum = a
+    for n in range(1, 170):
+        a *= (math.pow(-1, n) * math.pow(x, 2 * n + 1) * 2 * math.pow(n + 1, 2)) / ((2 * n + 1) * math.pow(-1, n + 1) * math.pow(x, 2 * n + 2))
+        sum += a
 
-    for n in range(0, 170):
-        sum += (math.pow(-1, n) * math.pow(x, 2 * n + 1)) / ((2 * n + 1) * (math.sqrt(2 * math.pi * n)
-                                                                            * math.pow(n, n)
-                                                                            * math.pow(math.e, n * (-1))))
+    res = 2 / math.sqrt(math.pi) * sum
 
-    result = 2 / math.sqrt(math.pi) * sum
-
-    print(f'erf({x}) = {result}')
+    print(f'erf({x}) = {res}')
