395534cd2de3cb3e6def0519b0363cf9b35ce706
aposum23 <misha.duhno@mail.ru>
2021-11-19 17:04:30 +0300

    Redaktirovanie

--- hardLevel.py
+++ hardLevel.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     a = math.pow(x * (-1), 3) / 3
     sum = a
     for n in range(1, 170):
-        a *= (math.pow(-1, n) * math.pow(x, 2 * n + 1) * 2 * math.pow(n + 1, 2)) / ((2 * n + 1) * math.pow(-1, n + 1) * math.pow(x, 2 * n + 2))
+        a *= ((2 * n + 1) * math.pow(x, 2)) / ((2 * n + 3) * (n + 1))
         sum += a
 
     res = 2 / math.sqrt(math.pi) * sum
